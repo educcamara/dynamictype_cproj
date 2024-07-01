0f509e56ca3434e96723a480d34626a4e9445cba
educcamara <github0520@eduardo.ccamara.com>
2024-06-30 23:37:32 -0400

    Added dynamictype.c/h

---
+++ include/dynamictype.h
@@ -0,0 +1,8 @@
+//
+// Created by Eduardo Cordeiro da Camara on 30/06/24.
+//
+
+#ifndef DYNAMICTYPE_H
+#define DYNAMICTYPE_H
+
+#endif //DYNAMICTYPE_H
---
+++ src/dynamictype.c
@@ -0,0 +1,5 @@
+//
+// Created by Eduardo Cordeiro da Camara on 30/06/24.
+//
+
+#include "dynamictype.h"
