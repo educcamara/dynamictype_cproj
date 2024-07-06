2f9cdc14e088e25727e767bd40f2f4b8e799792d
educcamara <eduardo.camara@icomp.ufam.edu.br>
2024-07-05 23:40:47 -0400

    Corrected tests for lse

--- tests/dynamictype_tests.c
+++ tests/dynamictype_tests.c
@@ -23,7 +23,7 @@ void test_create_dynamic_type() {
     assert(dynamictype != NULL);
     assert(strcmp(dynamictype->name, "Person") == 0);
     assert(dynamictype->fields != NULL);
-    assert(dynamictype->n_fields == 0);
+    assert(dynamictype->fields->tam == 0);
 
     printf("- create_dynamic_type() tests passed\n");
 }
@@ -32,13 +32,13 @@ void test_add_field() {
     tDynamicType *dynamictype = create_dynamic_type("Person");
     add_field(dynamictype, "name", "char*", "John Doe");
 
-    assert(dynamictype->n_fields == 1);
+    assert(dynamictype->fields->tam == 1);
     assert(strcmp(get_field(dynamictype, "name"), "John Doe") == 0);
 
     int num = 30;
     add_field(dynamictype, "age", "int", &num);
 
-    assert(dynamictype->n_fields == 2);
+    assert(dynamictype->fields->tam == 2);
     assert(*(int *) get_field(dynamictype, "age") == 30);
 
     printf("- add_field() tests passed\n");
