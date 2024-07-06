b07a331b40eca4095e4e6cecd0496c0e5afe8f36
educcamara <eduardo.camara@icomp.ufam.edu.br>
2024-07-06 11:22:15 -0400

    Wrote basic test structure

--- tests/dynamictypetemplate_tests.c
+++ tests/dynamictypetemplate_tests.c
@@ -1,3 +1,15 @@
 //
 // Created by Eduardo Cordeiro da Camara on 06/07/24.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "dynamictype.h"
+
+int main(void) {
+
+    printf("All tests passed on dynamictypetemplate_tests\n");
+    return 0;
+}
