84addfe7951b438c515650a8981ebe5cc170d807
educcamara <eduardo.camara@icomp.ufam.edu.br>
2024-07-06 11:38:42 -0400

    extended firsts test of dynamic templates

--- tests/dynamictypetemplate_tests.c
+++ tests/dynamictypetemplate_tests.c
@@ -9,22 +9,33 @@
 
 #include "dynamictype.h"
 
-void test_create_dynamic_type_template();
+void test_create_and_free_dynamic_type_template();
 
 int main(void) {
-    test_create_dynamic_type_template();
+    test_create_and_free_dynamic_type_template();
 
     printf("All tests passed on dynamictypetemplate_tests\n");
     return 0;
 }
 
-void test_create_dynamic_type_template() {
+void test_create_and_free_dynamic_type_template() {
     tDynamicTypeTemplate *dynamictype = create_dynamic_type_template("Person");
 
     assert(dynamictype != NULL);
     assert(strcmp(dynamictype->name, "Person") == 0);
     assert(dynamictype->fields != NULL);
     assert(dynamictype->fields->tam == 0);
+
+    free_dynamic_type_template(dynamictype);
+
+    tDynamicTypeTemplate *dynamictype2 = create_dynamic_type_template("String bigger than 20 characters");
+    assert(dynamictype2 != NULL);
+    assert(strcmp(dynamictype2->name, "String bigger than ") == 0);
+    assert(dynamictype2->fields != NULL);
+    assert(dynamictype2->fields->tam == 0);
+    assert(dynamictype2->fields->tam == 0);
+
+    free_dynamic_type_template(dynamictype2);
 
     printf("- create_dynamic_type_template() tests passed\n");
 }
